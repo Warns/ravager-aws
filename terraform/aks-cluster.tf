0a06d8f1c98e149f6de1387e1052a3fa6c39ad17
Mert <snickns@gmail.com>
2020-10-07 14:59:23 +0300

    updated aks-cluster

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -32,3 +32,11 @@ resource "azurerm_kubernetes_cluster" "dev-identity" {
     environment = "Dev Identity Service K8s"
   }
 }
+
+module "network" {
+  source = "Azure/network/azurerm"
+  resource_group_name = "azurerm_resource_group.dev-rg.name
+  address_space = "10.0.0.0/16"
+  subnet_prefixes = ["10.0.1.0/24"]
+  subnet_names = ["subnet1"]
+  depends_on = [azurerm_resource_group.dev-identity]
