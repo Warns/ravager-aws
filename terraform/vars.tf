eaa200d24c9c43e2a05105f2096b9250257ab101
Mert <snickns@gmail.com>
2020-10-07 11:01:33 +0300

    added vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,21 @@
+variable "location" {
+  type = string
+  default = "westeurope"
+}
+
+variable "prefix" {
+  type = string
+  default = "dev-identity"
+}
+
+variable "ssh-source-address" {
+  type = string
+  default = "*"
+
+variable "appId" {
+  description = ""
+}
+
+variable "password" {
+  description = ""
+}
