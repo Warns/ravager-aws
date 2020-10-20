9dfa459099e1ac4c378cca158a313f2191bb287d
Mert <snickns@gmail.com>
2020-10-20 15:57:09 +0300

    updated tstate bash

--- terraform/tstate.sh
+++ terraform/tstate.sh
@@ -10,3 +10,6 @@ az group create --name $RESOURCE_GROUP_NAME --location westeurope
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
+# Get storage account key
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+
