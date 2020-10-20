6644bb8db0d763a38ca9d48b6deaed55f05e62a0
Mert <snickns@gmail.com>
2020-10-20 15:53:32 +0300

    updated tstate bash

--- terraform/tstate.sh
+++ terraform/tstate.sh
@@ -7,3 +7,6 @@ CONTAINER_NAME=tstate
 # Create resource group
 az group create --name $RESOURCE_GROUP_NAME --location westeurope
 
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
