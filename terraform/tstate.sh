b9285a2aed1f96423a2a825d19cdf20fb23513a3
Mert <snickns@gmail.com>
2020-10-20 15:52:44 +0300

    updated tstate bash

--- terraform/tstate.sh
+++ terraform/tstate.sh
@@ -4,3 +4,6 @@ RESOURCE_GROUP_NAME=tstate-rg
 STORAGE_ACCOUNT_NAME=tstateidentity$RANDOM
 CONTAINER_NAME=tstate
 
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location westeurope
+
