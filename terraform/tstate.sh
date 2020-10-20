293f2c38c37e18ce295ae45ecd857f33b035304c
Mert <snickns@gmail.com>
2020-10-20 15:57:53 +0300

    updated tstate bash

--- terraform/tstate.sh
+++ terraform/tstate.sh
@@ -13,3 +13,6 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 # Get storage account key
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
 
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+
