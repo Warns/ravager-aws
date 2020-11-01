fe0e7cbe2ef88da98143979d804ccec017f17864
Mert <snickns@gmail.com>
2020-11-01 12:01:17 +0300

    create nginx ingress

--- terraform/aks-nginx-helm-lb.tf
+++ terraform/aks-nginx-helm-lb.tf
@@ -86,4 +86,24 @@ resource "helm_repository" "stable" {
   url  = "https://kubernetes-charts.storage.googleapis.com"
 }
 
+# Install Nginx Ingress using Helm Chart
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "${helm_repository.stable.metadata.0.name}"
+  chart      = "nginx-ingress"
 
+  set {
+    name  = "rbac.create"
+    value = "false"
+  }
+
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
+
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = "${azurerm_public_ip.nginx_ingress.ip_address}"
+  }
+}
