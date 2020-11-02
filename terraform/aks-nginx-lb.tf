63841a01d6934e2cd9b986e6d344d01f5972c90a
Mert Alnuaimi <snickns@gmail.com>
2020-11-02 20:41:51 +0300

    Update aks-nginx-lb.tf

--- terraform/aks-nginx-lb.tf
+++ terraform/aks-nginx-lb.tf
@@ -43,6 +43,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = "${azurerm_resource_group.aks.location}"
   resource_group_name = "${azurerm_resource_group.aks.name}"
   dns_prefix          = "${var.name_prefix}"
+# Change to match compliance
   kubernetes_version  = "1.11.3"
 
   agent_pool_profile {
