ff4388a53b06999fa19c9cb5d5593aabd146035a
Mert Alnuaimi <snickns@gmail.com>
2020-11-04 23:13:50 +0300

    Update nginxlb.tf

--- terraform/nginxlb.tf
+++ terraform/nginxlb.tf
@@ -1,18 +1,3 @@
-
-}
-
-# Initialize Helm (and install Tiller)
-provider "helm" {
-  install_tiller = true
-
-  kubernetes {
-    host                   = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
-    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)}"
-  }
-}
-
 # Create Static Public IP Address to be used by Nginx Ingress
 resource "azurerm_public_ip" "nginx_ingress" {
   name                         = "nginx-ingress-pip"
