d128994ddd644e7678db36c503ec878a9ee88d01
Mert Alnuaimi <snickns@gmail.com>
2020-11-04 22:58:22 +0300

    Update nginxlb.tf

--- terraform/nginxlb.tf
+++ terraform/nginxlb.tf
@@ -1,8 +1,3 @@
-# Create Azure AD Application for Service Principal
-resource "azurerm_azuread_application" "aks" {
-  name = "${var.name_prefix}-sp"
-}
-
 # Create Service Principal
 resource "azurerm_azuread_service_principal" "aks" {
   application_id = "${azurerm_azuread_application.aks.application_id}"
