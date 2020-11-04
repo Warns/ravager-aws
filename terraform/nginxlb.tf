e1645f43560f21157b81c0ee17b202422e6612ec
Mert Alnuaimi <snickns@gmail.com>
2020-11-04 23:16:18 +0300

    Update nginxlb.tf

--- terraform/nginxlb.tf
+++ terraform/nginxlb.tf
@@ -35,3 +35,11 @@
 # Initialize Helm (and install Tiller)
 provider "helm" {
   #  install_tiller = true
+
+    kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  }
+}
