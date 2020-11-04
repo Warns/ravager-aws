218734ac4636479fb2ec742d823bd58152f9246d
Mert Alnuaimi <snickns@gmail.com>
2020-11-04 22:57:16 +0300

    nginxlb init

--- terraform/nginxlb.tf
+++ terraform/nginxlb.tf
@@ -1,12 +1,4 @@
-# Set default name prefix
-variable "name_prefix" {
-  default = "k8s-cluster"
-}
 
-# Set default location
-variable "location" {
-  default = "westeurope"
-}
 
 # Create Resource Group
 resource "azurerm_resource_group" "aks" {
