7b4cdee7b1d3c91729b13b2ea3c0973f78674323
Mert Alnuaimi <snickns@gmail.com>
2020-11-04 23:01:34 +0300

    Update nginxlb.tf

--- terraform/nginxlb.tf
+++ terraform/nginxlb.tf
@@ -1,18 +1,8 @@
-# Create managed Kubernetes cluster (AKS)
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.name_prefix}-aks"
-  location            = "${azurerm_resource_group.aks.location}"
-  resource_group_name = "${azurerm_resource_group.aks.name}"
-  dns_prefix          = "${var.name_prefix}"
-# Change to match compliance
-  kubernetes_version  = "1.11.3"
-
-  agent_pool_profile {
+agent_pool_profile {
     name            = "linuxpool"
     count           = 3
     vm_size         = "Standard_DS2_v2"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
+
   }
 
   service_principal {
