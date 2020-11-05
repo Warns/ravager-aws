bbbb089e9956fd535bd7ebe55cfeea667c97309c
Mert Alnuaimi <snickns@gmail.com>
2020-11-05 22:21:42 +0300

    Update azuread.tf

--- terraform/azuread.tf
+++ terraform/azuread.tf
@@ -2,3 +2,9 @@ data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "config" {}
 
+# Generate random string to be used for service principal password
+resource "random_string" "password" {
+  length  = 32
+  special = true
+}
+
