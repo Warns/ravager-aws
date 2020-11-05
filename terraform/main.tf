fa4edca87c8755479b03d8260c1ce2a2710c01cf
Mert Alnuaimi <snickns@gmail.com>
2020-11-05 22:18:57 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,8 @@ provider "azurerm" {
   version = "=2.20.0"
   subscription_id = var.subscription_id
   client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 # create resource group
