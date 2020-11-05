af70b062ef3ef28e381bffd41d03b7e1532e9ad2
Mert Alnuaimi <snickns@gmail.com>
2020-11-05 22:22:07 +0300

    Update azuread.tf

--- terraform/azuread.tf
+++ terraform/azuread.tf
@@ -16,3 +16,10 @@ resource "azuread_service_principal" "aks" {
   application_id = azuread_application.aks.application_id
 }
 
+# Create service principal password
+resource "azuread_service_principal_password" "sp-password" {
+  end_date             = "2299-12-30T23:00:00Z" #forever
+  service_principal_id = azuread_service_principal.aks.id
+  value                = random_string.password.result
+}
+
